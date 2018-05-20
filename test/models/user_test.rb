34d77da197e13ee61731642b0bef5dda43db86ae
Iman <iman.mansour94@gmail.com>
2018-05-20 17:11:30 +0200

    add model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "user has email" do
+    user = User.new(email: "iman22@gmail.com")
+    assert user.email == "iman22@gmail.com"
+  end
+
+  test "should not save without gender" do
+    user = User.new
+    assert_not user.save, "saved the user without gender"
+  end
 end
